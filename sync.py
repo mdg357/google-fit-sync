beb437b62f88d5a25a09482fec6cbd2e4296b766
mdg357 <mdg357@gmail.com>
2016-05-26 13:00:23 -0500

    Updating logging and process flow

--- sync.py
+++ sync.py
@@ -98,9 +98,12 @@ def get_total_steps(data):
     """
     total_steps = 0
 
-    for entry in data['point']:
-        for value in entry['value']:
-            total_steps += value['intVal']
+    if 'point' in data:
+        for entry in data['point']:
+            for value in entry['value']:
+                total_steps += value['intVal']
+    else:
+        log_and_print("No step data found", True, 'warning')
 
     return total_steps
 
@@ -207,9 +210,10 @@ def increment_step_task(task_id, increment_value, habitica_credentials):
 def log_and_print(message, rollbar_log=None, rollbar_level=None):
     """Print the message to both the console and the log file.
     """
-    print(message)
-    logging.info(message)
-    
+    message_with_timestamp = "{0:%Y-%m-%d %H:%M:%S} - {1}".format(datetime.datetime.now(), message)
+    print(message_with_timestamp)
+    logging.info(message_with_timestamp)
+
     # Check whether or not to log this to rollbar
     if rollbar_log is None:
         rollbar_log = False
@@ -217,8 +221,8 @@ def log_and_print(message, rollbar_log=None, rollbar_level=None):
     # Check if the logging level is set
     if (rollbar_log == True) and (rollbar_level is None):
         rollbar_level = 'info'
-    
-    if rollbar_log == True:    
+
+    if rollbar_log == True:
         rollbar.report_message('Google Fitness Sync: ' + message, rollbar_level)
 
     return
@@ -238,18 +242,20 @@ def execute():
     timestamps = get_start_and_end_timestamps()
     data = get_fitness_data(timestamps)
     total_steps = get_total_steps(data)
-    increment_value = total_steps // STEP_DIVISOR
-    task_id = get_habitica_task(TASK_NAME, habitica_credentials)
 
-    log_and_print('Total Steps: {0}'.format(total_steps), True)
-    log_and_print('Task will be incremented {0} times'.format(increment_value))
+    if total_steps > 0:
+        increment_value = total_steps // STEP_DIVISOR
+        task_id = get_habitica_task(TASK_NAME, habitica_credentials)
 
-    # If the task exists, increment it
-    if task_id is None:
-        log_and_print("Task '{0}' does not exist.".format(TASK_NAME), True, 'error')
-    else:
-        log_and_print("Located '{0}' task.".format(TASK_NAME))
-        increment_step_task(task_id, increment_value, habitica_credentials)
+        log_and_print('Total Steps: {0}'.format(total_steps), True)
+        log_and_print('Task will be incremented {0} times'.format(increment_value))
+
+        # If the task exists, increment it
+        if task_id is None:
+            log_and_print("Task '{0}' does not exist.".format(TASK_NAME), True, 'error')
+        else:
+            log_and_print("Located '{0}' task.".format(TASK_NAME))
+            increment_step_task(task_id, increment_value, habitica_credentials)
 
     return
 
